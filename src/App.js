5427ba96c0d4f7f52539fdfdf55de65570b66655
foderking <freatshow@gmail.com>
2021-08-06 17:54:07 -0700

    added most functionality

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import InputField from './Functionality/InputField'
 import wiki from 'wikijs'
 import parseWiki from 'infobox-parser'
 import axios from 'axios'
+import RandomNumGen from './Functionality/random'
+import { useState } from 'react'
 
 const App = () =>
 {
   const song = InputField('text', '')
   const album = InputField('text', '')
+  // const producer = InputField('text', [])
+  const [producer, setproducer] = useState([])
 
   async function getPageKey(query)
   {
     const res = await axios.get(`https://en.wikipedia.org/w/rest.php/v1/search/title?q=${query}&limit=1`)
-    console.log(res.data.pages[0])
+    console.log('page key', res.data.pages[0])
 
     return res.data.pages[0].key
   }
@@ -20,16 +24,17 @@ const App = () =>
   async function getSource(key)
   {
     const res = await axios.get('https://en.wikipedia.org/w/rest.php/v1/page/' + key)
-    console.log(res.data)
+    console.log('source', res.data.source)
 
     return res.data.source
   }
 
   function getTracklist(source)
   {
-    let re = /{{tracklist.*?^}}/gms
+    let re = /{{track.*list.*?^}}/gmsi
 
-    return source.match(re)[0]
+    console.log('matched', source.match(re))
+    return source.match(re).length ?  source.match(re)[0] :  source.match(re)
   }
 
   function parseTracklist(tracklist)
@@ -38,9 +43,20 @@ const App = () =>
   }
 
   function getKeyByValue(object, value) {
-    return Object.keys(object).find(key =>
-      object[key].toLowerCase().contains(value.toLowerCase())
-    )
+    return Object.keys(object)
+      .find(key =>
+        String(object[key]).match(new RegExp(value, 'i'))
+      )
+  }
+
+  function getProducer(key, tracklist)
+  {
+    const k = key.slice(5)
+
+    const producer =tracklist[ 'extra' + k]
+    console.log('extra'+ k, tracklist, producer)
+
+    return producer
   }
 
 
@@ -53,10 +69,18 @@ const App = () =>
     const source = await getSource(key)
     // console.log(source)
 
-    const trackList = getTracklist(source)
-    console.log(parseTracklist(trackList))
+    const trackList = parseTracklist(getTracklist(source))
 
-    console.log(getKeyByValue(trackList, song.main.value))
+    console.log('parsed trackist', (trackList))
+
+    const valueKey = getKeyByValue(trackList, song.main.value)
+    console.log('key by value', valueKey)
+
+    const producer = getProducer(valueKey, trackList)
+
+    setproducer(producer)
+    // const map = {"first" : "1", "second" : "2"};
+// console.log(getKeyByValue(map,"2"))
     // wiki()
     //   .find(`${album.main.value} (album)`)
     //   .then(page => (page.html()))
@@ -93,6 +117,15 @@ const App = () =>
         </div>
         <button className='btn btn-secondary mb-2'>Search</button>
       </form>
+      <h2>Producer</h2>
+      <ul>
+      {
+        Array.isArray(producer) 
+        ? 
+        producer.map(each => <li key={RandomNumGen()}>{each}</li>)
+        : producer
+      }
+      </ul>
     </div>
   )
 }
