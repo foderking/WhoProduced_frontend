139499385587ccabc2ad6b52b7ea25966355a476
foderking <freatshow@gmail.com>
2021-09-29 15:07:32 +0100

    Refactored 'Index' view

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 
-export const Error = ({ error }) => {
+export const Error = ({ error , error_header }) => {
 
   return (
     <div className={`alert alert-danger alert-dismissible fade show ${error ? '' : 'd-none'}`} role="alert">
-      <strong>Error!</strong> {error}
+      <strong className="alert-heading" >{error_header}</strong>
+      <p>{error}</p>
       <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   )
--- src/Views/Index.js
+++ src/Views/Index.js
@@ -7,80 +7,109 @@ import { Error } from "../Components/Error";
 
 const DB_URL = 'https://whoproduced.herokuapp.com/search';
 
-export const Index = ({ spotify_filler, search }) => {
+export const Index = ({ spotify_filler, search }) =>
+{
   const show_albums = spotify_filler.state;   // 'empty' object to be populated when the request is successful
   const SetAlbums = spotify_filler.setState;
-  const [show_error, SetError] = useState('');
-  const [show_spinner, SetSpinner] = useState(false);
-  const [err_timeout, SetErrorTimeout] = useState(0)  // state for managing removal / timing out of the error 
 
-  const NewError = (message) => {
-    // Function modifies the "SetError" function and automatically sets "show_error" state back to ""  after a specified period of time
+  const [show_spinner, SetSpinner] = useState(false);
+
+  const [err_timeout, SetErrorTimeout] = useState(0)  // state for managing removal / timing out of the error 
+  const [show_error, SetError] = useState('');
+  const [error_header, SetErrorHeader] = useState("Error")
+
+  // Function modifies the "SetError" function and automatically sets "show_error" state back to ""  after a specified period of time
+  function NewError (error_message, error_header)
+  {
     const TTL = 5000
     clearInterval(err_timeout)  // resets any existing "show_error" state that hasn't already timed out to ""
-    SetError(message) // changes error message to specified error
+
+    // changes error message to specified error
+    SetError(error_message) // error message
+    SetErrorHeader(error_header) // error heading
 
     SetErrorTimeout(
       // resets back to "" after specified time
       setTimeout(() => SetError(''), TTL)
     )
   }
-  async function GetAlbums(url, data, headers)
-  {
-    try {
-      const response = await axios.post(url, data, headers);
-      return response
+
+  function ErrorAlias(err)
+  { // Error aliases to make them readable to the user
+    if (err === "EMPTY_SPOTIFY_RESPONSE") {
+      return "No Result From Spotify"
     }
-    catch (error) {
-      console.error("Error in 'GetAlbums()'", error)
-      SetError(error)
-      return
+    else return err
+  }
+
+  // The exception type for "Index" view
+  class IndexViewException {
+    constructor(message, exception_type)
+    {
+      this.message = message;
+      this.name = exception_type;
+    }
+    // Make the exception convert to a pretty string when used as a string
+    // (e.g., by the error console)
+    toString()
+    {
+      return `${this.name}: "${this.message}"`;
     }
   }
 
-  async function FindAlbum(request_query) {
-    console.log(`"FindAlbum()".Sending request to ${DB_URL} for "${request_query}"`);
-
+  async function GetAlbums(request_query, exception)
+  { // Handles query request to backend
+    console.log(`"GetAlbum()".Sending request to ${DB_URL} for "${request_query}"`);
     const request_headers = {
       "Content-Type": "application/json"
     };
     const request_data = {
       query: request_query,
       type: 'track',
-      limit: '5'
-    };
+    }
 
+    const response = await axios.post(DB_URL, request_data, request_headers)
+
+    console.log('"GetAlbum()" end.')
+
+    if (!response.data.tracks.items.length) { // exception when spotify doesn't find song
+      //  ** TODO ** : refactor error response  on backend
+      throw new exception("Spotify response didn't contain any result", "EMPTY_SPOTIFY_RESPONSE")
+    }   
+    else {
+      return response
+    }
+  }
+
+  async function FindAlbum(request_query)
+  {
+    console.log('start spinner')
+    SetSpinner(true);
+
+    console.log(`"FindAlbum()"`)
     try {
-      // sends request to server
-      const response = await axios.post(DB_URL, request_data, request_headers);
-
-      if (!response.data.tracks.items.length) {
-        NewError('No results');
-        // SetError('No results');
-        console.log("FindAlbum Error: No results")
-        return
-      }
+      const response = await GetAlbums(request_query, IndexViewException)     // sends request to server
       SetAlbums(response.data);
       console.log('"FindAlbum()" successful. response ->', response.data);
     }
     catch (e) {
-      console.log(e);
-      console.error('Error in "FindAlbum"', e);
+      console.error(e);
+      NewError(e.message, ErrorAlias(e.name) )
     }
+    console.log('"FindAlbum()" end.')
+    
+    console.log('stop spinner')
+    SetSpinner(false)
   }
 
   async function HandleSubmit(e) {
     console.log("'handlesubmit()'. initial object ->", show_albums);
     e.preventDefault();
-    // clearTimeout(err_timeout)
 
-    console.log('start spinner')
-    SetSpinner(true);
 
     await FindAlbum(search.main.value)
     
-    console.log('stop spinner')
-    SetSpinner(false)
+    console.log('"handleSubmit()" end.')
   }
 
 
@@ -109,9 +138,13 @@ export const Index = ({ spotify_filler, search }) => {
           </form>
         </div>
 
+        {/*
+        * Spinner component - shows when the webiste is still searching or loading
+        *  Error component - shows errors
+        */}
         <div className='hh'>
-          <Spinner show={show_spinner} />
-          <Error error={show_error} />
+          <Spinner show={show_spinner} /> 
+          <Error error={show_error} error_header={error_header} />    
           {
             show_albums.tracks.items.map(each => <Card key={each.id} each={each} />)
           }
